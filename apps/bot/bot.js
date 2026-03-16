634324b526d8c1786d1faf5e9d1b4dd6eaa372c9
Manuel Moran <manu.moran0710@gmail.com>
2026-03-16 16:03:14 -0300

    create user and group member functionality added. expenses ongoing

--- apps/bot/bot.js
+++ apps/bot/bot.js
@@ -4,6 +4,13 @@ import { supabase } from '../../supabaseclient.js';
 
 
 class SplitBot {
+    static createUser = async (ctx) => {
+      const newUser = { telegram_id:ctx.from.id, name:ctx.from.first_name }
+      await SplitBot.addData(newUser, 'users')
+
+      ctx.reply('Hola 😎 Ya estoy vivo')
+    }
+
     static showExpense(ctx) {
         const userId = ctx.from.id
         const name = ctx.from.first_name  
@@ -21,30 +28,14 @@ class SplitBot {
         ctx.reply(texto)
     }
     
-    static saveExpense(ctx) {
-        const userId = ctx.from.id
-        const name = ctx.from.first_name
-
+    static createExpense(ctx) {
         const args = ctx.message.text.split(' ').slice(1)
 
         const amount = args[0]
-        const concepto = args.slice(1).join(' ')
-        const description = concepto.charAt(0).toUpperCase() + concepto.slice(1)
+        const concept = args.slice(1).join(' ')
+        const description = concept.charAt(0).toUpperCase() + concept.slice(1)
 
-        
-        if (!userExpenses[userId]) {
-          userExpenses[userId] = {
-            name,
-            expenses: []
-          }
-        }
-        
-        userExpenses[userId].expenses.push({
-          amount,
-          description
-        })
-
-        ctx.reply(`Gasto guardado ${name}`)
+        userId, groupId = this.getIds(ctx)
     }
 
     static createGroup = async (ctx) => {
@@ -64,28 +55,54 @@ class SplitBot {
       }
 
       const newGroup = { name:groupName, code:groupCode }
-      await SplitBot.addData(newGroup)
+      await SplitBot.addData(newGroup, 'groups')
 
       ctx.reply(`Grupo ${groupName} creado`)
       ctx.reply(`Codigo: ${groupCode}`)
+    }
+
+    static getIds = async(ctx, groupCode) => {
+      const userQuery = supabase
+        .from("users")
+        .select("id")
+        .eq("telegram_id", ctx.from.id)
+        .single()
+
+      const groupQuery = supabase
+        .from("groups")
+        .select("id, name")
+        .eq("code", groupCode)
+        .single()
+
+      const [
+        { data: dataUser },
+        { data: dataGroup }
+      ] = await Promise.all([userQuery, groupQuery])
+
+      return {
+        userId: dataUser.id,
+        groupId: dataGroup.id,
+        groupName: dataGroup.name
+      }
     }
 
     static joinGroup = async(ctx) => {
       const args = ctx.message.text.split(' ').slice(1)
       const groupCode = args.join(' ')
 
-      const { data, error } = await supabase.from("groups").select("name").eq('code', groupCode)
-      
-      if(data.length) {
-        const newUser = { name:groupName, code:groupCode }
-        ctx.reply(`Te uniste al grupo: ${data[0].name}`)
+      const { userId, groupId, groupName } = await SplitBot.getIds(ctx, groupCode)
+
+      if(groupName) {
+        const newGroupMember = { user_id:userId, group_id:groupId }
+        await SplitBot.addData(newGroupMember, 'group_members')
+        ctx.reply(`Te uniste al grupo: ${groupName}`)
       } else {
         ctx.reply(`No existe grupo con el codigo: ${groupCode}`)
       }
     }
 
-    static addData = async(data) => {
-      const { error } = await supabase.from('groups').insert(data);
+    static addData = async(data, table) => {
+      const { error } = await supabase.from(table).insert(data);
       return error;
     }
 }
@@ -95,7 +112,7 @@ import { Telegraf } from 'telegraf'
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.command('gastar', (ctx) => {
-  SplitBot.saveExpense(ctx)
+  SplitBot.createExpense(ctx)
 })
   
 bot.command('listar', (ctx) => {
@@ -111,10 +128,10 @@ bot.command('unirse', (ctx) => {
 })
 
 bot.start((ctx) => {
-  ctx.reply('Hola 😎 Ya estoy vivo')
+  SplitBot.createUser(ctx)
 })
   
-bot.hears('hola', (ctx) => {
+bot.hears(/hola/i, (ctx) => {
   ctx.reply('Tu nariz contra mis bolas')
 })
 
